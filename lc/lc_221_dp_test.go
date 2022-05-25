5fad606d07dc78ea37e8ae9c108a9265e32651a5
Arseni Kavalchuk <ars.kov@gmail.com>
2022-05-25 13:54:37 +0300

    Leetcode: 221. Maximal Square

---
+++ lc/lc_221_dp_test.go
@@ -0,0 +1,68 @@
+package lc
+
+import (
+	"testing"
+
+	"github.com/ArseniKavalchuk/dsa-go/pkg/mymath"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLeetcode221_1_0(t *testing.T) {
+	matrix := [][]byte{
+		{'0'},
+	}
+	res := maximalSquare(matrix)
+	assert.Equal(t, 0, res)
+}
+
+func TestLeetcode221_1_1(t *testing.T) {
+	matrix := [][]byte{
+		{'1'},
+	}
+	res := maximalSquare(matrix)
+	assert.Equal(t, 1, res)
+}
+
+func TestLeetcode221_1_2(t *testing.T) {
+	matrix := [][]byte{
+		{'0', '1'},
+		{'1', '0'},
+	}
+	res := maximalSquare(matrix)
+	assert.Equal(t, 1, res)
+}
+
+func TestLeetcode221_1_3(t *testing.T) {
+	matrix := [][]byte{
+		{'1', '0', '1', '0', '0'},
+		{'1', '0', '1', '1', '1'},
+		{'1', '1', '1', '1', '1'},
+		{'1', '0', '0', '1', '0'},
+	}
+	res := maximalSquare(matrix)
+	assert.Equal(t, 4, res)
+}
+
+func maximalSquare(matrix [][]byte) int {
+	m := len(matrix)
+	if m == 0 {
+		return 0
+	}
+	n := len(matrix[0])
+	dp := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	maxSize := 0
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if matrix[i-1][j-1] == '1' {
+				dp[i][j] = mymath.Min(mymath.Min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+				if dp[i][j] > maxSize {
+					maxSize = dp[i][j]
+				}
+			}
+		}
+	}
+	return maxSize * maxSize
+}

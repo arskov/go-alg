9a19ebfc41d4c504606f2ffb3c32c34db1927ed1
Arseni Kavalchuk <ars.kov@gmail.com>
2022-06-04 22:11:50 +0300

    Leetcode: 121. Best Time to Buy and Sell Stock

---
+++ lc/lc_121_buy_and_sell_1_test.go
@@ -0,0 +1,38 @@
+package lc
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLeetcode121_0_1(t *testing.T) {
+	prices := []int{7, 1, 5, 3, 6, 4}
+	res := maxProfit(prices)
+	assert.Equal(t, 5, res)
+}
+
+func TestLeetcode121_0_2(t *testing.T) {
+	prices := []int{7, 6, 4, 3, 1}
+	res := maxProfit(prices)
+	assert.Equal(t, 0, res)
+}
+
+func maxProfit(prices []int) int {
+	n := len(prices)
+	if n == 0 {
+		return 0
+	}
+	minSoFar := math.MaxInt32
+	diff := 0
+	for _, v := range prices {
+		if v < minSoFar {
+			minSoFar = v
+		}
+		if v-minSoFar > diff {
+			diff = v - minSoFar
+		}
+	}
+	return diff
+}

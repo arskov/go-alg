d980d52cb00c5ac233c02a9cb781d42960ae0fbd
Arseni Kavalchuk <ars.kov@gmail.com>
2022-06-04 22:13:49 +0300

    Fix for avoiding a conflict with function names

--- lc/lc_121_buy_and_sell_1_test.go
+++ lc/lc_121_buy_and_sell_1_test.go
@@ -9,17 +9,17 @@ import (
 
 func TestLeetcode121_0_1(t *testing.T) {
 	prices := []int{7, 1, 5, 3, 6, 4}
-	res := maxProfit(prices)
+	res := maxProfit_1(prices)
 	assert.Equal(t, 5, res)
 }
 
 func TestLeetcode121_0_2(t *testing.T) {
 	prices := []int{7, 6, 4, 3, 1}
-	res := maxProfit(prices)
+	res := maxProfit_1(prices)
 	assert.Equal(t, 0, res)
 }
 
-func maxProfit(prices []int) int {
+func maxProfit_1(prices []int) int {
 	n := len(prices)
 	if n == 0 {
 		return 0

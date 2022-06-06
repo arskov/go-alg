1763e0a24fbd117631126dc2b7154e456d422264
Arseni Kavalchuk <ars.kov@gmail.com>
2022-06-07 01:00:06 +0300

    Leetcode: 122. Best Time to Buy and Sell Stock II (DP)

---
+++ lc/lc_122_buy_and_sell_2_dp_test.go
@@ -0,0 +1,63 @@
+package lc
+
+import (
+	"testing"
+
+	"github.com/ArseniKavalchuk/dsa-go/pkg/mymath"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLeetcode122_1_1(t *testing.T) {
+	prices := []int{7, 1, 5, 3, 6, 4}
+	res := maxProfit_2_dp(prices)
+	assert.Equal(t, 7, res)
+}
+
+func TestLeetcode122_1_2(t *testing.T) {
+	prices := []int{1, 2, 3, 4, 5}
+	res := maxProfit_2_dp(prices)
+	assert.Equal(t, 4, res)
+}
+
+func TestLeetcode122_1_3(t *testing.T) {
+	prices := []int{7, 6, 4, 3, 1}
+	res := maxProfit_2_dp(prices)
+	assert.Equal(t, 0, res)
+}
+
+//[1,2,3,1,2,3]
+
+func maxProfit_2_dp(prices []int) int {
+	n := len(prices)
+	if n == 0 {
+		return 0
+	}
+
+	memo := make([][]int, 2)
+	memo[0] = make([]int, n)
+	memo[1] = make([]int, n)
+	for i := 0; i < n; i++ {
+		memo[0][i] = -1
+		memo[1][i] = -1
+	}
+
+	var dp func(txActive int, i int) int
+	dp = func(txActive int, i int) int {
+		if i == n {
+			return 0
+		}
+		if memo[txActive][i] == -1 {
+			if txActive == 0 {
+				a := -prices[i] + dp(txActive^1, i+1)
+				b := dp(txActive, i+1)
+				memo[txActive][i] = mymath.Max(a, b)
+			} else {
+				a := prices[i] + dp(txActive^1, i+1)
+				b := dp(txActive, i+1)
+				memo[txActive][i] = mymath.Max(a, b)
+			}
+		}
+		return memo[txActive][i]
+	}
+	return dp(0, 0)
+}

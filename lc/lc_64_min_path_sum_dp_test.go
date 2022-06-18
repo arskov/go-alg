e17a7eeb55ea09fbc57b30b4dcc16a60b66cc465
Arseni Kavalchuk <ars.kov@gmail.com>
2022-06-19 00:24:43 +0300

    Leetcode: 64. Minimum Path Sum

---
+++ lc/lc_64_min_path_sum_dp_test.go
@@ -0,0 +1,52 @@
+package lc
+
+import (
+	"testing"
+
+	"github.com/ArseniKavalchuk/dsa-go/pkg/mymath"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLeetcode64_1_0(t *testing.T) {
+	grid := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+	res := minPathSum(grid)
+	assert.Equal(t, 12, res)
+}
+
+func TestLeetcode64_1_2(t *testing.T) {
+	grid := [][]int{
+		{1, 3, 1},
+		{1, 5, 1},
+		{4, 2, 1},
+	}
+	res := minPathSum(grid)
+	assert.Equal(t, 7, res)
+}
+
+func minPathSum(grid [][]int) int {
+	m := len(grid)
+	if m == 0 {
+		return 0
+	}
+	n := len(grid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < m; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+	for i := 1; i < n; i++ {
+		dp[0][i] = dp[0][i-1] + grid[0][i]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = grid[i][j] + mymath.Min(dp[i-1][j], dp[i][j-1])
+		}
+	}
+	return dp[m-1][n-1]
+}

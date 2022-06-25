d0ca817d6035fddd95c917edf88a5fbef3fc0706
Arseni Kavalchuk <ars.kov@gmail.com>
2022-06-25 21:51:11 +0300

    Leetcode: 256. Paint House

---
+++ lc/lc_256_paint_house_1_dp_test.go
@@ -0,0 +1,54 @@
+package lc
+
+import (
+	"testing"
+
+	"github.com/ArseniKavalchuk/dsa-go/pkg/mymath"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLeetcode256_1_0(t *testing.T) {
+	costs := [][]int{
+		{17, 2, 17},
+		{16, 16, 5},
+		{14, 3, 19},
+	}
+	res := minCost_256(costs)
+	assert.Equal(t, 10, res)
+}
+
+func TestLeetcode256_1_1(t *testing.T) {
+	costs := [][]int{
+		{7, 6, 2},
+	}
+	res := minCost_256(costs)
+	assert.Equal(t, 2, res)
+}
+
+func minCost_256(costs [][]int) int {
+	n := len(costs)
+	if n == 0 {
+		return 0
+	}
+	memo := make([][]int, 3)
+	memo[0] = make([]int, n)
+	memo[1] = make([]int, n)
+	memo[2] = make([]int, n)
+	for i := 0; i < n; i++ {
+		memo[0][i] = -1
+		memo[1][i] = -1
+		memo[2][i] = -1
+	}
+	var dp func(step, nextColor int) int
+	dp = func(step, nextColor int) int {
+		if step == n {
+			return 0
+		}
+		if memo[nextColor][step] == -1 {
+			memo[nextColor][step] = costs[step][nextColor] +
+				mymath.Min(dp(step+1, (nextColor+1)%3), dp(step+1, (nextColor+2)%3))
+		}
+		return memo[nextColor][step]
+	}
+	return mymath.Min(dp(0, 0), mymath.Min(dp(0, 1), dp(0, 2)))
+}
